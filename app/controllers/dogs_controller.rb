639c6105ff6a5097bda2a660f75893f31863da1e
rpdecks <rpdecks@gmail.com>
2020-04-28 11:55:48 -0400

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,16 @@
 class DogsController < ApplicationController
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+    @dog = Dog.find_by(id: params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
 end
